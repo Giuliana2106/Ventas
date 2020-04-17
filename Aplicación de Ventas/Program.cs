21cc025b72a67b89b449076145279158a2fbb875
Giuliana2106 <62736506+Giuliana2106@users.noreply.github.com>
2020-04-16 21:33:18 -0600

    Se trabajo primera parte del proyecto

---
+++ Aplicación de Ventas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aplicación_de_Ventas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string nit;
+            double sub;
+            string pro1;
+            double pre1;
+            int cant1;
+            string linea;
+
+            Console.Write("Ingrese el nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingrese Nit: ");
+            nit = Console.ReadLine();
+
+            sub1 = 0;
+
+
+            Console.ReadKey();
+        }
+    }
+}
